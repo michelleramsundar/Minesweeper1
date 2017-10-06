d74418408d8262abc2b70092442c5f0541c98d5a
Michelle Ramsundar <32465303+michelleramsundar@users.noreply.github.com>
2017-10-06 16:08:54 -0700

    Update minesweeper.js

--- minesweeper.js
+++ minesweeper.js
@@ -1,4 +1,17 @@
 "use strict";
 
-var board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
-console.log(board);
+
+const board = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+];
+
+const printBoard = (board) => {
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
+
+
+printBoard(board);
